8387aa045db476ad5d22c48c1d0c5529f8243e4b
Michael <micumieu@gmail.com>
2017-09-18 23:27:38 -0700

    Add forms, action sheets, toasts, models and services.

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,25 +1,118 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup , FormControl, Validators, FormArray} from '@angular/forms';
 
-/**
- * Generated class for the EditRecipePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { NavParams, ActionSheetController, AlertController, ToastController } from 'ionic-angular';
 
-@IonicPage()
+
+
 @Component({
   selector: 'page-edit-recipe',
   templateUrl: 'edit-recipe.html',
 })
-export class EditRecipePage {
+export class EditRecipePage implements OnInit {
+  mode = 'New';
+  selectOptions = ['Easy', 'Medium', 'Hard'];
+  recipeForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor (private navParams: NavParams,
+              private actionSheetController: ActionSheetController,
+              private alertCtrl: AlertController,
+              private toastCtrl: ToastController) {}
+
+  ngOnInit() {
+    this.mode = this.navParams.get('mode');
+    this.initializeForm();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EditRecipePage');
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  onManageIngredients() {
+    const actionSheet = this.actionSheetController.create({
+      title: 'What do you want to do?',
+      buttons: [
+        {
+          text: 'Add Ingredient',
+          handler: () => {
+            this.createNewIngredientAlert().present();
+          }
+        },
+        {
+          text: 'Remove all Ingredients',
+          role: 'destructive',
+          handler: () => {
+            const fArray: FormArray = <FormArray>this.recipeForm.get('ingredients');
+            const len = fArray.length;
+            if (len > 0 ) {
+              for (let i = len -1; i >= 0; i--) {
+                fArray.removeAt(i);
+              } 
+            const toast = this.toastCtrl.create({
+                message: 'All Ingredients were deleted!',
+                duration: 1500,
+                position: 'bottom'
+              });
+              toast.present();
+            }
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  private createNewIngredientAlert() {
+    return this.alertCtrl.create({
+      title: 'Add Ingredient',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        }
+      ],
+        buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Add',
+          handler: data => {
+            if(data.name.trim() == '' || data.name == null) {
+              const toast = this.toastCtrl.create({
+                message: 'Please enter a valid value!',
+                duration: 1500,
+                position: 'bottom'
+              });
+              toast.present();
+              return;
+            }
+            (<FormArray>this.recipeForm.get('ingredients'))
+              .push(new FormControl(data.name, Validators.required));
+              const toast = this.toastCtrl.create({
+                message: 'Item Added!',
+                duration: 1500,
+                position: 'bottom'
+              });
+              toast.present();
+          }
+        }
+      ]
+    });
+  }
+
+  private initializeForm() {
+    this.recipeForm = new FormGroup({
+      'title': new FormControl(null, Validators.required),
+      'description': new FormControl(null, Validators.required),
+      'difficulty': new FormControl('Medium', Validators.required),
+      'ingredients': new FormArray([])
+    });
   }
 
 }
--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,25 +1,35 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
 
-/**
- * Generated class for the ShoppingListPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { ShoppingListService } from '../../services/shopping-list';
+import { Ingredient } from '../../models/ingredient';
 
-@IonicPage()
+
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html',
 })
 export class ShoppingListPage {
+  listItems: Ingredient[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private slService: ShoppingListService ) {}
+
+  ionViewWillEnter() {
+    this.loadItems();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ShoppingListPage');
+  onAddItem(form: NgForm) {
+    this.slService.addItem(form.value.ingredientName, form.value.amount);
+    form.reset();
+    this.loadItems();
   }
 
+  onCheckItem(index: number) {
+    this.slService.removeItem(index);
+    this.loadItems();
+  }
+
+  private loadItems() {
+    this.listItems = this.slService.getItems();
+  }
 }
